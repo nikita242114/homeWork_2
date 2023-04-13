2cf3752d2faddf4459780015a6d905abaa7a3a44
nikita242114 <5358711@mail.ru>
2023-04-13 16:42:07 +0500

    start

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*
+456 ->5
+782 ->8
+918 ->1
+*/
+
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
